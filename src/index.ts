d37487fd1e379cf297994de627772c3ada0d0e4b
bkjonathan <thomas@Thomass-iMac-Pro.local>
2024-01-18 21:39:30 +0630

    Publish Message

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-function good(){
-  console.log('god')
-  const interFun = ()=>{
-    console.log('I am internal function')
-  }
+import express, { Express } from 'express'
+import bodyParser from 'body-parser'
+import { Producer } from './producer'
 
-  interFun()
-}
+const app = express()
 
-good()
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello from Rabbit MQ'
+  })
+})
+app.post('/sendLog', async (req, res, next) => {
+  const producer = new Producer()
+  await producer.publishMessage(req.body.logType, req.body.message)
+  res.send()
+})
+
+app.listen(3000, () => {
+  console.log('Server start at http://localhost:3000')
+})
---
+++ src/producer.ts
@@ -0,0 +1,37 @@
+import ampq, { Channel } from 'amqplib'
+
+export class Producer {
+  private readonly config = {
+    // url: 'amqp://localhost:15672',
+    url: 'amqp://root:rootPwd@localhost',
+    exchangeName: 'logExchange'
+  }
+  private channel: any = null
+
+  async createChannel() {
+    try {
+      const connection = await ampq.connect(this.config.url)
+      this.channel = await connection.createChannel()
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  async publishMessage(type: string, message: string): Promise<void> {
+    try {
+      if (!this.channel) {
+        await this.createChannel()
+      }
+      await this.channel.assertExchange(this.config.exchangeName, 'direct')
+
+      await this.channel.publish(
+        this.config.exchangeName,
+        type,
+        Buffer.from(JSON.stringify({ type, message, dateTime: Date.now() }))
+      )
+      console.log(`This message ${message} is send to exchange ${this.config.exchangeName}`)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
